95ef3770b48c475645f0785aeb86e452a5034bc3
Nasra-Hendricks <Nasrahen@gmail.com>
2024-11-08 10:16:22 +0200

    added files

---
+++ Exercise 1.py
@@ -0,0 +1,41 @@
+# Question 1: Variable Assignment and String Manipulation
+
+print("Hey user, what is your name and age?")
+username = input('Enter your name: ')
+user_var = username
+print("The name entered is " + username)
+
+user_age = input('Enter your age: ')
+age = user_age
+print("The age entered is " + age)
+
+print("It is nice to meet you" " " + username)
+user= input("I can't believe you are" " " + age)
+#------------------------------------------------------------------------------------
+# Question 2: Integer Operations
+
+print("Hey user,what is the length and width of a rectangle?")
+length= int (input("length: "))
+user_length= length
+
+width= int (input("width: "))
+user_width= width
+
+area= (length * width)
+print("area=", + area)
+
+
+
+#------------------------------------------------------------------------------------
+# Question 3: Working with Floats
+
+print("Hey user,what is the temperature in Celsuis")
+celsuis= float (input("Celsius: "))
+user_temperature = celsuis
+
+fahrenheit= (celsuis * 9/5) + 32
+print("Faranheit=", +fahrenheit )
+
+
+
+
